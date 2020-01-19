32b0e28aeb7a4b92e017584db93b644fb215fa89
Simon Cho <a09730040@gmail.com>
2020-01-20 07:47:21 +0800

    Make baseline

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score, precision_score, roc_auc_score
 import lightgbm as lgb
 import matplotlib.pyplot as plt
 
@@ -36,7 +36,8 @@ def print_feature_importance(feature_name, feature_importance):
         print(e)
 
 def feature_engineering(train_transaction, test_transaction, category_col):
-    drop_col = ["id_07", "id_08", "id_21", "id_22", "id_23", "id_24", "id_25", "id_26", "id_27", "id_18", "D7", "dist2", "D13", "D14"]
+    """
+    drop_col = ["card4", "addr2", "id_17", "id_18", "id_22", "id_24", "id_27", "id_29"]
 
     for col in train_transaction.columns:
         if(1.0 * train_transaction[col].isnull().sum() / train_transaction.shape[0] > 0.5):
@@ -48,10 +49,12 @@ def feature_engineering(train_transaction, test_transaction, category_col):
     for col in drop_col:
         if(col in category_col):
             category_col.remove(col)
+    """
 
     dataset = train_transaction.append(test_transaction, ignore_index=True, sort=False)
     dataset = label_encoding(dataset, category_col)
 
+    """
     START_DATE = '2017-12-01'
     startdate = datetime.datetime.strptime(START_DATE, "%Y-%m-%d")
     dataset["Date"] = dataset["TransactionDT"].apply(lambda x: (startdate + datetime.timedelta(seconds=x)))
@@ -59,12 +62,15 @@ def feature_engineering(train_transaction, test_transaction, category_col):
     dataset["Hours"] = dataset["Date"].dt.hour
     dataset["Days"] = dataset["Date"].dt.day
     dataset.drop("Date", inplace=True, axis=1)
+    """
 
-    dummy_col = ["card4", "card6", "M1", "M2", "M3", "M4", "M6", "Weekdays", "Hours", "Days"]
+    """
+    dummy_col = ["card4"]
     dataset = pd.get_dummies(dataset, columns=dummy_col)
     for col in dummy_col:
         if(col in category_col):
             category_col.remove(col)
+    """
 
     train_transaction = dataset[:train_transaction.shape[0]]
     test_transaction = dataset[train_transaction.shape[0]:]
@@ -89,7 +95,7 @@ def train(train_transaction, label, test_transaction, category_col):
         x_valid = train_transaction.iloc[valid_idx]
         y_valid = label.iloc[valid_idx]
 
-        clf = lgb.LGBMClassifier(num_leaves=60, min_child_samples=60, subsample_freq=1, subsample=0.8, colsample_bytree=0.8, reg_alpha=0.8, metric=None, learning_rate=0.1, n_estimators=100)
+        clf = lgb.LGBMClassifier(num_leaves=60, min_child_samples=60, subsample_freq=1, subsample=0.8, colsample_bytree=0.8, reg_alpha=0.8, metric=None, learning_rate=0.1, n_estimators=1000, early_stopping_rounds=100)
         clf.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_valid, y_valid)], categorical_feature=category_col, eval_metric="auc")
 
         valid_predict = clf.predict_proba(x_valid)[:, 1]
